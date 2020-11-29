f0767ca35865a8eec920a6db90c304b7c3ce857f
Selma Wanna <slwanna@utexas.edu>
2020-11-28 18:27:02 -0700

    finished all functions expect perplexity and abstractratio. need to add datasets to path and begin analyzing

--- VLNChoiSurvey/main.py
+++ VLNChoiSurvey/main.py
@@ -1,63 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import stanza
 import os
 import complexity
+# from nltk.tree import *
+# import nltk
+from nltk.util import ngrams
+from nltk.util import pad_sequence
+from collections import Counter
 from nltk.parse.corenlp import CoreNLPServer
 from nltk.parse import *
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag_sents
+from nltk import FreqDist
 
-Dataset = list[str]
+
+def getVocabSize(sentences):
+    word_counter = Counter()
+    for sentence in sentences:
+        tokenized_sentence = word_tokenize(sentence)
+        if word_counter is not None:
+            for word in tokenized_sentence:
+                word_counter[word] += 1.0
+    return len(word_counter)
 
 
-def getVocabSize(dataset: Dataset):
+def getMeanFrazier(server, sentences):
+    with server:
+        parser = CoreNLPParser()
+        sentence_list = []
+        for sentence in sentences:
+            parse = next(parser.raw_parse(sentence))
+            sentence_list.append(str(parse[0]))
+    server.stop()
+    return complexity.get_mean_frazier(sentence_list)
+
+
+def getMeanYngve(server, sentences):
+    with server:
+        parser = CoreNLPParser()
+        sentence_list = []
+        for sentence in sentences:
+            parse = next(parser.raw_parse(sentence))
+            sentence_list.append(str(parse[0]))
+    server.stop()
+    return complexity.get_mean_yngve(sentence_list)
+
+
+def getPoSDistribution(sentences):
+    sentences_tokenized = [word_tokenize(i) for i in sentences]
+    pos_tagged_sentences = pos_tag_sents(sentences_tokenized)
+    pos_tags = []
+    for sentence in pos_tagged_sentences:
+        pos_tags += [pos_tag for _, pos_tag in sentence]
+    fd = FreqDist(pos_tags)
+    return fd.most_common(5)
+
+
+def getAbstractRatio(sentences):
     raise NotImplementedError
 
 
-def getMeanFrazier(dataset: Dataset):
-    # TODO: Use helper functions in complexity.py
-    raise NotImplementedError
+def getAvgSentLen(sentences):
+    sentence_length_list = []
+    for sentence in sentences:
+        num_words = len(word_tokenize(sentence))
+        sentence_length_list.append(num_words)
+    total_words = sum(sentence_length_list)
+    num_sentences = len(sentence_length_list)
+    return total_words/num_sentences
 
 
-def getMeanYngve(dataset: Dataset):
-    # TODO: Use helper functions in complexity.py
-    raise NotImplementedError
-
-
-def getPoSDistribution(dataset: Dataset):
-    raise NotImplementedError
-
-
-def getAbstractRatio(dataset: Dataset):
-    raise NotImplementedError
-
-
-def getAvgSentLen(dataset: Dataset):
-    raise NotImplementedError
-
-
-def getPerplexity(dataset: Dataset):
+def getPerplexity(sentences):
     raise NotImplementedError
 
 
 if __name__ == '__main__':
-    #
-    # TODO: FOR HANDLING EXCEPTIONS
-    #
-    # https://bbengfort.github.io/snippets/2018/06/22/corenlp-nltk-parses.html
-    #
-    # jars = (
-    #     "stanford-corenlp-3.9.1.jar",
-    #     "stanford-corenlp-3.9.1-models.jar"
-    # )
-    #
-    # with CoreNLPServer(*jars):
-    #     parser = CoreNLPParser()
-    #
-    #     text = "The runner scored from second on a base hit"
-    #     parse = next(parser.parse_text(text))
-
     working_directory = os.getcwd()
     core_nlp_directory = os.path.join(working_directory, 'stanford-corenlp-4.2.0')
 
@@ -66,15 +81,8 @@ if __name__ == '__main__':
         os.path.join(core_nlp_directory, "stanford-corenlp-4.2.0-models.jar"),
     )
 
-    server.start()
-
-    parser = CoreNLPParser()
-    parse = next(parser.raw_parse("I put the book in the box on the table."))
-
-    pee = complexity.word_score(parse)
-    server.stop()
-    print(pee)
-
-# TODO:
-# Download Lingual Aspects of Datasets and convert their lingual components into a list of strings.
-# Feed list of strings into each function
+    example = ['Mary had a little lamb',
+               'Jack went up the hill',
+               'Jill followed suit',
+               'i woke up suddenly',
+               'it was a really bad dream.']
